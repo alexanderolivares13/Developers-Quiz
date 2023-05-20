00b30ff1c003ef188e0a391512490c3a2c120d70
alexanderolivares13 <124226984+alexanderolivares13@users.noreply.github.com>
2023-05-20 12:00:56 -0700

    created transitions for 4 questions that change and give feedback to the user if they pick the correct/wrong answer

--- assets/script.js
+++ assets/script.js
@@ -4,20 +4,23 @@
 // the first question will be prompted by pushing by unhiding the questions element
 // the remaining questions will be prompted by the textContent being filled after answering a question
 
-
-var option1El = document.querySelector(".option-1")
-var option2El = document.querySelector(".option-2")
-var option3El = document.querySelector(".option-3")
-var option4El = document.querySelector(".option-4")
-var timerEL = document.querySelector("#timer")
-var startPageEl = document.querySelector(".start-page")
-var startButton = document.querySelector(".start-button")
-var timerSeconds;
+var questionhEl = document.querySelector(".question-h2");
+var correctEl = document.querySelector(".correct");
+var wrongEl = document.querySelector(".wrong");
+var option1El = document.querySelector(".option-1");
+var option2El = document.querySelector(".option-2");
+var option3El = document.querySelector(".option-3");
+var option4El = document.querySelector(".option-4");
+var timerEL = document.querySelector("#timer");
+var startPageEl = document.querySelector(".start-page");
+var questionsEl = document.querySelector(".questions");
+var endPageEl = document.querySelector(".end-page")
+var startButton = document.querySelector(".start-button");
+var timerSeconds = 90;
 var timer;
 var started = false;
 
 function countdown (){
-    timerSeconds = 90;
     timer = setInterval(function () {
         timerSeconds--;
         timerEL.textContent = "Time: " + timerSeconds;
@@ -36,6 +39,81 @@ startButton.addEventListener("click", function(event) {
         return;
     }
     started = true;
-    startPageEl.setAttribute("style", "display: none;")
+    startPageEl.setAttribute("style", "display: none;");
+    questionsEl.setAttribute("style", "display: block;");
     countdown();
+});
+
+option1El.addEventListener("click", function(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    if (option1El.textContent === "1. Strings") {
+        correctEl.textContent = "Correct!";
+// Question 3
+        questionhEl.textContent = "What can a variable consist of?";
+        option1El.textContent = "1. Functions";
+        option2El.textContent = "2. Arrays";
+        option3El.textContent = "3. Booleans";
+        option4El.textContent = "4. All of the above";
+
+    } else if (option1El.textContent !== "1. Strings") {
+        wrongEl.textContent = "Wrong! Please try again";
+    } 
+    if (option1El.textContent === "1. Yes") {
+
+    }
+
+});
+
+option2El.addEventListener("click", function(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    if (option2El.textContent === "2. Parenthesis") {
+        correctEl.textContent = "Correct!";
+// Question 2.
+        questionhEl.textContent = "What data type is specified by quotations?";
+        option1El.textContent = "1. Strings";
+        option2El.textContent = "2. Numbers";
+        option3El.textContent = "3. Booleans";
+        option4El.textContent = "4. Objects";
+
+    } else if (option2El.textContent !== "2. Parenthesis") {
+        wrongEl.textContent = "Wrong! Please try again!";
+        timerSeconds - 15;
+    }
+});
+
+option3El.addEventListener("click", function(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    if (option3El.textContent === "3. Yes in green") {
+        clearInterval(timer)
+        questionsEl.setAttribute("style", "display: none;")
+        endPageEl.setAttribute("style", "display: block;")
+
+    } else if (option3El.textContent !== "3. Yes in green") [
+        wrongEl.textContent = "Wrong! Please try again!";
+    ]
+});
+
+option4El.addEventListener("click", function(event) {
+    event.preventDefault();
+    event.stopPropagation();
+
+    if (option4El.textContent === "4. All of the above") {
+        correctEl.textContent = "Correct!";
+// Question 4
+        questionhEl.textContent = "Are arrays, functions, and expressions objects?";
+        option1El.textContent = "1. Yes";
+        option2El.textContent = "2. No";
+        option3El.textContent = "3. Yes in green";
+        option4El.textContent = "4. No in red";
+        option1El.setAttribute ("style", "background-color: white;")
+        option2El.setAttribute ("style", "background-color: white;")
+        option3El.setAttribute ("style", "background-color: green;")
+        option4El.setAttribute ("style", "background-color: red;")
+
+    } else if (option4El.textContent !== "4. All of the above") {
+        wrongEl.textContent = "Wrong! Please try again";
+    }
 });
