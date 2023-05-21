2365b4831d2f2ec76b4f382a3b94e03731796009
alexanderolivares13 <124226984+alexanderolivares13@users.noreply.github.com>
2023-05-21 15:41:03 -0700

    added more comments. added comments to the index.html and to the highscores.js script

--- assets/highscores.js
+++ assets/highscores.js
@@ -2,6 +2,7 @@ var tableLine1 = document.querySelector("#line-1");
 var gobackButton = document.querySelector("#go-back");
 var clearButton = document.querySelector("#clear");
 
+// Declaring the function that will run on page load. The number1 and score1 variable will get the score and name of the user that was saved into the local data from the main page and display them.
 function renderHighscore () {
     var number1 = localStorage.getItem("score");
     var score1 = localStorage.getItem("name");
@@ -9,15 +10,13 @@ function renderHighscore () {
 }
 
 renderHighscore()
-
-clearButton.addEventListener("click", function(event) {
-    event.preventDefault();
+// The clearButton will set the local storage to nothing and will clear the highscore shown on the page
+clearButton.addEventListener("click", function() {
     localStorage.setItem("name", "");
     localStorage.setItem("score", "");
     tableLine1.textContent = "";
 })
-
-gobackButton.addEventListener("click", function(event) {
-    event.preventDefault();
-    window.location.href = ("../index.html")
+// The gobackButton will take the user back to the main index page and will let them answer the questions again.
+gobackButton.addEventListener("click", function() {
+    window.location.href = ("../index.html");
 })
